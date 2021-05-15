2482adb522e869defd084abece4e5ac5961623a0
Lam Nguyen <lamcnguyen89@gmail.com>
2021-05-15 18:20:56 -0400

    Added the code for a Side Menu on the user screen and it shows up correctly. But I know I didn't do it right. It's a start though.

--- Project_2/UserViewController.swift
+++ Project_2/UserViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import SideMenu
 
 class UserViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var menu: SideMenuNavigationController?
     var quizArray = DBHelper.inst.getQuiz()
     var quizNames = [String]()
-    
-    
-    
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -60,9 +60,18 @@ class UserViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewD
         
         DBHelper.inst.holdCurrentQuiz(name: quizNames[0])
         
+        // Create Side Menu
+        menu = SideMenuNavigationController(rootViewController: MenuListController())
+        menu?.leftSide = true
+        menu?.setNavigationBarHidden(true, animated: false)
+        SideMenuManager.default.leftMenuNavigationController = menu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
     }
     
-
+    @IBAction func didTapMenu() {
+        present(menu!, animated: true)
+    }
+    
     @IBAction func quiz(_ sender: Any) {
         var user = DBHelper.inst.getOneUser(user: DBHelper.inst.current)
         print(String(user.qtaken))
@@ -79,14 +88,36 @@ class UserViewController: UIViewController,  UIPickerViewDelegate, UIPickerViewD
         }
         
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+class MenuListController: UITableViewController {
+    
+    var items = ["First","Second","Third","First","Second","Third"]
+    
+    let darkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = darkColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = darkColor
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Do something
+    }
+    
 }
